ede72d16eaf6fbcfff3a12cb1790510ab714579c
enversaglam <enversaglam@gmail.com>
2024-11-12 21:39:35 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+import City from './City';
 
 function App() {
+  const key ="eba434d53498ed908a3e822f4e3bce27";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
+  useEffect(() =>{
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  },[search])
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+        onChange={(e)=> setSearch(e.target.value)}
+        type="text"
+        placeholder="Placeholder"
+        class="mt-5 py-1 w-[250px] placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring"/>
+        <City city={city} />
+      </div>
     </div>
   );
 }
